858c0991ac2c1ed5687f63a2b95007c2371eb2fa
Sanskar <sanskarin@outlook.in>
2026-08-19 14:38:26 +0530

    refactor: implement question store contract

--- lib/src/data/question_repository.dart
+++ lib/src/data/question_repository.dart
@@ -1,12 +1,14 @@
 import '../domain/question.dart';
 import 'app_database.dart';
 import 'demo_questions.dart';
+import 'question_store.dart';
 
-final class QuestionRepository {
+final class QuestionRepository implements QuestionStore {
   const QuestionRepository(this.database);
 
   final AppDatabase database;
 
+  @override
   Future<List<Question>> loadAll() async {
     final List<Question> questions = await database.loadQuestions();
     if (questions.isNotEmpty) {
@@ -17,6 +19,7 @@ final class QuestionRepository {
     return database.loadQuestions();
   }
 
+  @override
   Future<void> saveAll(Iterable<Question> questions) =>
       database.upsertQuestions(questions);
 }
