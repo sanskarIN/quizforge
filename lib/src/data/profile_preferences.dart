27e09074bd3bc133a1141898e9d3628117037768
Sanskar <sanskarin@outlook.in>
2026-08-19 14:38:46 +0530

    refactor: implement profile selection store

--- lib/src/data/profile_preferences.dart
+++ lib/src/data/profile_preferences.dart
@@ -1,6 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
-final class ProfilePreferences {
+import 'profile_selection_store.dart';
+
+final class ProfilePreferences implements ProfileSelectionStore {
   ProfilePreferences({SharedPreferencesAsync? preferences})
       : _preferences = preferences ?? SharedPreferencesAsync();
 
@@ -8,11 +10,14 @@ final class ProfilePreferences {
 
   static const String _activeProfileKey = 'profiles.activeId';
 
+  @override
   Future<String?> loadActiveProfileId() =>
       _preferences.getString(_activeProfileKey);
 
+  @override
   Future<void> saveActiveProfileId(String profileId) =>
       _preferences.setString(_activeProfileKey, profileId);
 
+  @override
   Future<void> clearActiveProfileId() => _preferences.remove(_activeProfileKey);
 }
