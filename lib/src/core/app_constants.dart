0a859b1cc7b0b3bd24166261a46a5dfaf807d500
Sanskar <sanskarin@outlook.in>
2026-08-19 08:33:36 +0530

    feat: centralize product identity constants

---
+++ lib/src/core/app_constants.dart
@@ -0,0 +1,12 @@
+abstract final class AppConstants {
+  static const String appName = 'QuizForge';
+  static const String version = '0.1.0';
+  static const String credit = 'Made by the Sanskar';
+
+  static const String githubUrl = 'https://github.com/sanskarIN/quizforge';
+  static const String githubProfileUrl = 'https://github.com/sanskarIN';
+  static const String buyMeACoffeeUrl = 'https://buymeacoffee.com/sanskarIN';
+  static const String businessEmail = 'sanskarin@outlook.in';
+  static const String secondaryBusinessEmail = 'sanskarin.business@gmail.com';
+  static const String supportEmail = 'supportramsandesh@gmail.com';
+}
