741d9d5187bd115f27cda1439ac16d2f3402670c
Sanskar <sanskarin@outlook.in>
2026-08-19 11:23:44 +0530

    chore: include documentation links in PowerShell checks

--- tool/check.ps1
+++ tool/check.ps1
@@ -3,5 +3,6 @@ $ErrorActionPreference = "Stop"
 flutter pub get
 flutter gen-l10n
 dart format --output=none --set-exit-if-changed lib test tool
+python tool/check_markdown_links.py
 flutter analyze
 flutter test --coverage
